b792c0edd10d0af979495bd9e5864082c3c5f64a
chenthehero <ericchen67@gmail.com>
2020-02-24 21:58:44 -0500

    Added users and link db association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_024312) do
+ActiveRecord::Schema.define(version: 2020_02_25_025751) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
